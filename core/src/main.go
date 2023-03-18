ceed88719dc584eec9205ee7f76174f68ceddf6e
2020mini.fsuke1985 <fsuke1985@gmail.com>
2023-03-18 21:16:07 +0900

    update

--- core/src/main.go
+++ core/src/main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	googleimage "github.com/fsuke1985/jacreen/core/types"
+	"encoding/json"
+	"io/ioutil"
+	"os"
+	"reflect"
+)
 
 func main() {
-	fmt.Println("golang")
+	var x *googleimage.GoogleImage
+	var j googleimage.GoogleImage
+
+	raw, err := ioutil.ReadFile("../data.json")
+	if err != nil {
+		os.Exit(1)
+	}
+
+	json.Unmarshal(raw, &x)
+
+	kJson := reflect.TypeOf(j)
+	// vJson := reflect.ValueOf(x)
+
+	for i := 0; i < kJson.NumField(); i++ {
+		// f := kJson.Field(i)
+		v, b := kJson.FieldByName("Code")
+		
+		fmt.Println(v.Code)
+	}
 }
